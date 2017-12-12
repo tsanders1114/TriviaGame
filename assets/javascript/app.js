c4f0ca70c1ac5cf7fc4d72234c0c1b9bbd8fb87d
PlugInPlaya <tsanders1114@gmail.com>
2017-12-11 23:36:14 -0500

    First Commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+
+
+function submitAnswers(){
+	var total = 5;
+	var score = 0;
+	var q1 = document.forms["quizForm"]["q1"] .value;
+	var q2 = document.forms["quizForm"]["q2"] .value;
+	var q3 = document.forms["quizForm"]["q3"] .value;
+	var q4 = document.forms["quizForm"]["q4"] .value;
+	var q5 = document.forms["quizForm"]["q5"] .value;
+	
+	
+}
+
+alert(q1);
+
+/*for(i = 1; i <= total; i ++){
+	if(eval ('q' + i) == null || eval ('q1' + 1) ==''){
+	
+	}
+};
+
+var answers = ["b","a","c","d","a"]
+
+for(i = 1; i<= total; i++){
+	if(evak('q1' + i)== answers[i - 1]){
+		score++;
+	}
+};
+
+var results = document.getElementById('results');
+
+results.innerHTML= "<h2>You Scored"<span>''+ score''</span> out of <span>'+total+'</span></h2>"
+
+	alert("You scored" + score + "out of" + total");
+return false
+*/
