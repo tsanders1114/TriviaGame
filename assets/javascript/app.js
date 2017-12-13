24e6f84703d9ca224dd6cbf3e02e1f580216491a
PlugInPlaya <tsanders1114@gmail.com>
2017-12-12 23:17:21 -0500

    Third Changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,33 @@
 
 
+
 function submitAnswers(){
 	var total = 5;
 	var score = 0;
-	var q1 = document.forms["quizForm"]["q1"] .value;
-	var q2 = document.forms["quizForm"]["q2"] .value;
-	var q3 = document.forms["quizForm"]["q3"] .value;
-	var q4 = document.forms["quizForm"]["q4"] .value;
-	var q5 = document.forms["quizForm"]["q5"] .value;
+	var q1 = document.forms["quizform"]["q1"] .value;
+	var q2 = document.forms["quizform"]["q2"] .value;
+	var q3 = document.forms["quizform"]["q3"] .value;
+	var q4 = document.forms["quizform"]["q4"] .value;
+	var q5 = document.forms["quizform"]["q5"] .value;
 	
-	
-}
-
-alert(q1);
-
-/*for(i = 1; i <= total; i ++){
-	if(eval ('q' + i) == null || eval ('q1' + 1) ==''){
-	
+	for(i = 1; i <= total; i ++){
+		if(eval('q' + i) == null || eval('q' + i) ==''){
+			alert("You Missed question"  +  i);
+			return false;
+		}
 	}
+	
+	var answers =["a","b","c","b","c"];
+	for(i = 1; i <= total; i ++)
+		if('q'+ 1) == answers[i-1]{
+			score++;
+	
+		}
+	}		
+var results= document.getElementById("#results");
+results.innerHTML="<h3>You scored <span> + score + </span> out of <span> + total + '</span></h3>";
+ 	alert("You scored"  + score +  "out of  + total");
+		
+			
 };
 
-var answers = ["b","a","c","d","a"]
-
-for(i = 1; i<= total; i++){
-	if(evak('q1' + i)== answers[i - 1]){
-		score++;
-	}
-};
-
-var results = document.getElementById('results');
-
-results.innerHTML= "<h2>You Scored"<span>''+ score''</span> out of <span>'+total+'</span></h2>"
-
-	alert("You scored" + score + "out of" + total");
-return false
-*/
